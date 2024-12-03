c3233509ac73c63c6b797a10b7728e26b141ae1d
vladibo13 <vladibo13@gmail.com>
2024-12-03 20:45:35 +0200

    testing env variables

--- app.js
+++ app.js
@@ -11,18 +11,35 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        }
+    );
+      console.log(`MongoDb connected ${conn.connection.host}`);
+    } catch (error) {
+      console.log(`Error ${error.message}`);
+      process.exit(1);
     }
-})
+  };
+
+  connectDB()
+
+// mongoose.connect('', {
+
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, function(err) {
+//     if (err) {
+//         console.log("error!! " + err)
+//     } else {
+//       //  console.log("MongoDB Connection Successful")
+//     }
+// })
 
 var Schema = mongoose.Schema;
 
